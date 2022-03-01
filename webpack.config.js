4fa34d5d01e2fca384bb89c9141d4ec03b10a6ca
Callum H Stewart <chs1@hw.ac.uk>
2022-03-01 11:16:28 +0000

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,36 @@
- const path = require('path');
- const HtmlWebpackPlugin = require('html-webpack-plugin');
- const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
- module.exports = {
-   watch: true,
-   mode: 'development',
-   entry: [
-    './src/main.js',
-   ],
-   devtool: 'inline-source-map',
-
+module.exports = {
+  watch: true,
+  mode: 'development',
+  entry: [
+   './src/main.js',
+  ],
+  devtool: 'inline-source-map',
   devServer: {
-
     static: [
-	{directory: path.join(__dirname, './dist')}, 
+        {directory: path.join(__dirname, './dist')}, 
       {
-	directory: path.join(__dirname, './public'),
-	publicPath: '/public'	
+        directory: path.join(__dirname, './public'),
+        publicPath: '/public'	
       }],
-
   },
-   devtool: 'inline-source-map',
-   plugins: [
-    new CopyWebpackPlugin({
-      patterns: [
-        { from: 'public', to: 'pubilc' },
-      ],
-    }),
-    new HtmlWebpackPlugin({
-      template: 'src/index.html'
-    })
-   ],
-   output: {
-     filename: '[name].bundle.js',
-     path: path.resolve(__dirname, 'dist'),
-     clean: true,
-   },
- };
+  devtool: 'inline-source-map',
+  plugins: [
+   new CopyWebpackPlugin({
+     patterns: [
+       { from: 'public', to: 'pubilc' },
+     ],
+   }),
+   new HtmlWebpackPlugin({
+     template: 'src/index.html'
+   })
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+};
